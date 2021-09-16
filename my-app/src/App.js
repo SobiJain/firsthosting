25e79341b62fdcb0665d6b7f78db7c0077cfd898
SobiJain <sobi3jain1@gmail.com>
2021-09-16 18:14:39 +0530

    button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Sync your customers</h1>
+        <button id="myButton" class="float-left submit-button">Enable Workflow</button>
     </div>
   );
 }
