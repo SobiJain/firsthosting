bbd8b3662310e2252aa0818b7ccc1ebe6d1dab43
SobiJain <sobi3jain1@gmail.com>
2021-09-19 14:33:11 +0530

    enable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 function App() {
   function auth() {
-    window.location.href = "https://api.thecodemesh.online/api/v1/enable/user-workflow/6146b9a4c0dc290012eb4d05/" + window.location.search;
+    window.location.href = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/" + window.location.search;
   }
   return (
     <div>
