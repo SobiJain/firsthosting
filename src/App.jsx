2e18ae6d6982d8de97684ab598be97e83c989cd6
SobiJain <sobi3jain1@gmail.com>
2021-09-19 19:22:13 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
+import axios from "axios";
+
 function App() {
   function auth() {
-    // window.location.href = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/" + window.location.search;
     const url = 'https://api.thecodemesh.online/api/v1/enable/user-workflow/6146b9a4c0dc290012eb4d05/'+window.location.search;
-    window.open(url,'_blank');
+    axios.get(url)
+      .then(function (response) {
+        // handle success
+        console.log("jaideep success!! : ", response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("oh no bro error", error);
+      });
+  }
+  function enable(){
+        const enableURL = 'https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/'+window.location.search;
+        axios.get(enableURL)
+      .then(function (response) {
+
+        // handle success
+        console.log("success in enableWorkFlow : ", response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error in enableWorkFlow : ", error);
+      });  
+  }
+  function runSync() {
+    const runURL = "https://api.thecodemesh.online/api/v1/trigger/user-workflow/6146b9a4c0dc290012eb4d05";
+    axios.get(runURL)
+      .then(function (response) {
+
+        // handle success
+        console.log("success in sync : ", response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error in sync : ", error);
+      });
   }
   return (
     <div>
-      <h1>Sync your customers</h1>
-      <button id="myButton" className="float-left submit-button" onClick={auth}>Enable Workflow</button>
+      <button className="float-left submit-button" onClick={auth}>auth</button>
+      <br></br>
+      <button className="float-left submit-button" onClick={enable}>enable/</button>
+      <br></br>
+      <button className="float-left submit-button" onClick={runSync}>runSync</button>
+      <br></br>
     </div>
   );
 }
