93f63421bf0ee5429a87b7da5b8f1c2915ca646d
SobiJain <sobi3jain1@gmail.com>
2021-09-19 14:58:47 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ function App() {
   function auth() {
     window.location.href = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/" + window.location.search;
   }
+
+  window.open( 'https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/'+window.location.search, '_blank' )
+
   return (
-      window.open( 'https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/'+window.location.search, '_blank' ),
     <div>
       <h1>Sync your customers</h1>
       <button id="myButton" className="float-left submit-button" onClick={auth}>Enable Workflow</button>
