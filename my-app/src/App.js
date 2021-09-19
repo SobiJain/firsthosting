bb8760754fb2390e80a7ea9ba9582bb266e56faa
SobiJain <sobi3jain1@gmail.com>
2021-09-19 10:37:34 +0530

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-      console.log("entered"),
-    <div>
-        <h1>Sync your customers</h1>
-        <button id="myButton" class="float-left submit-button">Enable Workflow</button>
-    </div>
-  );
+    console.log("henlo");
 }
 
 export default App;
