7b2320f1b034ba6a7dbccf80bc4cd2ef52182211
SobiJain <sobi3jain1@gmail.com>
2021-09-19 14:35:25 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 function App() {
-  function auth() {
     window.location.href = "https://api.thecodemesh.online/api/v1/enable/enable-user-workflow/6146b9a4c0dc290012eb4d05/" + window.location.search;
+  function auth() {
+    
   }
   return (
     <div>
