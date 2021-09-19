e81c515f79b9ba9e9dfe1350228bf96d873440ad
SobiJain <sobi3jain1@gmail.com>
2021-09-19 10:34:07 +0530

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 function App() {
   return (
+      console.log("entered"),
     <div>
         <h1>Sync your customers</h1>
         <button id="myButton" class="float-left submit-button">Enable Workflow</button>
