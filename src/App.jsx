de05bdc4cf722dbc22e64413e81610fcdc5e2d91
SobiJain <sobi3jain1@gmail.com>
2021-09-30 14:53:34 +0530

    changed url format

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 function App() {
   function auth() {
       const params = new Map(window.location.search.slice(1).split('&').map(kv => kv.split('=')));
-    const url = 'https://api.thecodemesh.online/api/v1/auth/user-worfkflow/6146b9a4c0dc290012eb4d05/?shop='+params.get('shop');
+    const url = 'https://api.thecodemesh.online/api/v1/auth/user-workflow/6146b9a4c0dc290012eb4d05/?shop='+params.get('shop');
     axios.get(url)
       .then(function (response) {
         // handle success
@@ -16,7 +16,7 @@ function App() {
   }
   function enable(){
         const params = new Map(window.location.search.slice(1).split('&').map(kv => kv.split('=')));
-        const enableURL = 'https://api.thecodemesh.online/api/v1/enable/user-worfkflow/6146b9a4c0dc290012eb4d05/?username='+params.get('shop');
+        const enableURL = 'https://api.thecodemesh.online/api/v1/enable/user-workflow/6146b9a4c0dc290012eb4d05/?username='+params.get('shop');
         axios.get(enableURL)
       .then(function (response) {
 
